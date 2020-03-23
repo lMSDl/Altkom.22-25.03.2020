c7f3d363791d1183f81ec354e0331608b49f6341
Paweł Łęczycki <pawel@msd.info.pl>
2020-03-23 09:42:42 +0000

    DesignPrinciples

--- WPCSharp/Program.cs
+++ WPCSharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DesignPrinciples;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,17 @@ namespace WPCSharp
     {
         static void Main(string[] args)
         {
+            var service = new Class2();
+
+            var customerId = 1;
+
+            service.Func2(customerId, 500);
+            if (service.Func1(customerId, 100))
+                Console.WriteLine($"Customer {customerId} charged. Actual balance: {service.GetBalance(customerId)}");
+            else
+                Console.WriteLine($"Not enought founds on customer {customerId} acconut! Actual balance: {service.GetBalance(customerId)}");
+
+            Console.ReadKey();
         }
     }
 }
