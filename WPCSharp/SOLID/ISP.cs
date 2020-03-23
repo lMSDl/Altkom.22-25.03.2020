26e8185241ad6eb168c32724cb8344c59dd0235d
Paweł Łęczycki <pawel@msd.info.pl>
2020-03-23 12:46:13 +0000

    ISP

---
+++ WPCSharp/SOLID/ISP.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SOLID
+{
+    interface IExcelFormatter
+    {
+        void ToExcel();
+    }
+
+    interface IPdfFormatter
+    {
+        void ToPdf();
+    }
+
+    class Report : IPdfFormatter, IExcelFormatter
+    {
+        public void ToExcel()
+        {
+            Console.WriteLine("Excel generated");
+        }
+
+        public void ToPdf()
+        {
+            Console.WriteLine("Pdf generated");
+        }
+    }
+
+    class Poem : IPdfFormatter
+    {
+        public void ToPdf()
+        {
+            Console.WriteLine("Pdf generated");
+        }
+    }
+}
