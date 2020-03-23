7c884aa94f910619a1bc281e1ce311d662261fd2
Paweł Łęczycki <pawel@msd.info.pl>
2020-03-23 12:00:30 +0000

    OCP

---
+++ WPCSharp/SOLID/OCP.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+    abstract class Shape
+    {
+        public abstract int Area();
+    }
+
+    class Square : Shape
+    {
+        public int A { get; set; }
+
+        public override int Area()
+        {
+            return A * A;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+
+        public override int Area()
+        {
+            return A * B;
+        }
+    }
+
+
+
+    class ShapeCalculator
+    {
+        public int Area(Shape shape)
+        {
+            return shape.Area();
+        }
+    }
+}
