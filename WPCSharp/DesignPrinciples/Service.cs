251de725660a46868c67bf457fe2184715c3e97f
Paweł Łęczycki <pawel@msd.info.pl>
2020-03-23 10:03:24 +0000

    DRY

--- WPCSharp/DesignPrinciples/Service.cs
+++ WPCSharp/DesignPrinciples/Service.cs
@@ -20,13 +20,18 @@ namespace DesignPrinciples
             return Customers.SingleOrDefault(x => x.AllowedDebit == allowedDebit);
         }
 
+        public Customer FindCustomerById(int customerId)
+        {
+           return Customers.SingleOrDefault(x => x.Id == customerId);
+        }
+
         public bool Change(int id, float amount)
         {
-            var customer = Customers.SingleOrDefault(x => x.Id == id);
+            var customer = FindCustomerById(id);
             if (customer == null)
                 return false;
 
-            if (customer.Income - customer.Outcome + customer.AllowedDebit < amount)
+            if (GetBalance(id) < amount)
                 return false;
 
             customer.Outcome += amount;
@@ -35,7 +40,7 @@ namespace DesignPrinciples
 
         public void Fund(int customerId, float amount)
         {
-            var customer = Customers.Where(x => x.Id == customerId).SingleOrDefault();
+            var customer = FindCustomerById(customerId);
             if (customer == null)
                 return;
             customer.Income += amount;
@@ -43,8 +48,8 @@ namespace DesignPrinciples
 
         public float? GetBalance(int customerId)
         {
-            var customer = Customers.Where(x => x.Id == customerId).SingleOrDefault();
-            return customer?.Income - customer?.Outcome;
+            var customer = FindCustomerById(customerId);
+            return customer?.Income - customer?.Outcome + customer.AllowedDebit;
         }
     }
 }
