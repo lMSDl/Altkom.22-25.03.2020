45374e9eb796227f617f80478c6670f7c31e63c7
Paweł Łęczycki <pawel@msd.info.pl>
2020-03-24 10:50:16 +0000

    Prototype

---
+++ WPCSharp/CreationalDesignPatterns/Prototype/Client.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CreationalDesignPatterns.Prototype
+{
+    public class Client
+    {
+        public static Dictionary<string, JsonSerializerSettings> JsonSettings = new Dictionary<string, JsonSerializerSettings>();
+
+        public static void Execute()
+        {
+            var mother = new Person()
+            {
+                Name = "Anna Annowska",
+            };
+
+            var person = new Person()
+            {
+                Name = "Daniel Danielski",
+                BirthDate = new DateTime(1978, 2, 12),
+                IdInfo = new IdInfo()
+            };
+
+            person.Mother = mother;
+            //mother.Kids.Add(person);
+
+
+            var personCopy = (Person)person.Clone();
+
+            person.IdInfo.Metadata = "abc";
+            person.Name = "Daniel Danielek";
+
+            person = personCopy;
+
+
+
+            var jsonSettings = new JsonSerializerSettingsClonable();
+            jsonSettings.DateParseHandling = DateParseHandling.DateTimeOffset;
+            jsonSettings.Formatting = Formatting.Indented;
+            jsonSettings.NullValueHandling = NullValueHandling.Include;
+
+            JsonSettings["FormattingIntendedDateTimeOffset"] = jsonSettings;
+            jsonSettings = (JsonSerializerSettingsClonable)jsonSettings.Clone();
+            jsonSettings.Formatting = Formatting.None;
+            JsonSettings["FormattingNoneDateTimeOffset"] = jsonSettings;
+
+
+        }
+
+        public class JsonSerializerSettingsClonable : JsonSerializerSettings, ICloneable
+        {
+            public object Clone()
+            {
+                return MemberwiseClone();
+            }
+        }
+    }
+}
---
+++ WPCSharp/CreationalDesignPatterns/Prototype/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationalDesignPatterns.Prototype
+{
+    public class Person : ICloneable
+    {
+        public Person()
+        {
+
+        }
+
+        public DateTime BirthDate { get; set; }
+        public string Name { get; set; }
+        public IdInfo IdInfo { get; set; }
+        public Person Mother { get; set; }
+
+        public ICollection<Person> Kids { get; set; } = new List<Person>();
+
+
+        public object Clone()
+        {
+            var person = (Person)ShallowClone();
+            person.IdInfo = (IdInfo)IdInfo?.Clone();
+            person.Mother = (Person)Mother?.Clone();
+            person.Kids = Kids.Select(x => x.Clone()).Cast<Person>().ToList();
+            return person;
+        }
+
+        public object ShallowClone()
+        {
+            return MemberwiseClone();
+        }
+    }
+
+    public class IdInfo : ICloneable
+    {
+        public int IdNumber { get; }
+        public string Metadata { set; get; }
+
+        public IdInfo()
+        {
+            IdNumber = new Random().Next(1000, 100000);
+        }
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+}
