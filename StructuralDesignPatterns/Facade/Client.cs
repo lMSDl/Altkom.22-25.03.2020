061d54a85de3692fac5d4b7dac5dca88b682ee69
Paweł Łęczycki <pawel@msd.info.pl>
2020-03-24 13:26:05 +0000

    Facade

---
+++ StructuralDesignPatterns/Facade/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace StructuralDesignPatterns.Facade
+{
+    public class Client
+    {
+        public static void Execute()
+        {
+            var settings = new JsonSerializerSettingsFacade();
+            settings.NullValueHandling = NullValueHandling.Ignore;
+            settings.ReferenceLoopHandling = ReferenceLoopHandling.Serialize;
+            Serialize(new object, settings);
+        }
+
+        public static void Serialize(object x, JsonSerializerSettingsFacade settings)
+        {
+            //...
+        }
+    }
+}
---
+++ StructuralDesignPatterns/Facade/JsonSerializerSettingsFacade.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructuralDesignPatterns.Facade
+{
+    public class JsonSerializerSettingsFacade
+    {
+        private JsonSerializerSettings _jsonSerializerSettings;
+
+        public JsonSerializerSettingsFacade()
+        {
+            _jsonSerializerSettings = new JsonSerializerSettings();
+            _jsonSerializerSettings.DateFormatString = "yyy.MM.dd";
+            _jsonSerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Local;
+
+        }
+
+        public NullValueHandling NullValueHandling { get => _jsonSerializerSettings.NullValueHandling; set => _jsonSerializerSettings.NullValueHandling = value; }
+        public ReferenceLoopHandling ReferenceLoopHandling { get => _jsonSerializerSettings.ReferenceLoopHandling; set => _jsonSerializerSettings.ReferenceLoopHandling = value; }
+    }
+}
